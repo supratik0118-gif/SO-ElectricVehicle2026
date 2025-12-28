78b56d773258a6821dcbe42175a64eecd851e879
Supratik Biswas <supratik0118@gmail.com>
2025-12-28 18:14:54 -0500

    Code with 2 sec run

--- SO-ElectricVehicle2026.ino
+++ SO-ElectricVehicle2026.ino
@@ -18,8 +18,11 @@
 
 Adafruit_NeoPixel pixels(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int count_left;
-int count_right;
+volatile unsigned int count_left;
+volatile unsigned int count_right;
+unsigned int count_left_buff;
+unsigned int count_right_buff;
+bool run = 0;
 void setup() {
   // put your setup code here, to run once:
   pinMode(0, OUTPUT);
@@ -29,9 +32,13 @@ void setup() {
   count_left = 0;
   count_right = 0;
   Serial.begin(9600);
-  attachInterrupt(2, readEncoder_left, CHANGE);
-  attachInterrupt(3, readEncoder_right, CHANGE);
+  attachInterrupt(2, readEncoder_left, FALLING);
+  attachInterrupt(3, readEncoder_right, FALLING);
+  //noInterrupts();
   pixels.begin();
+  pixels.setPixelColor(0, pixels.Color(100, 0, 0));
+  pixels.show();
+  //noInterrupts();
 
 }
 
@@ -45,14 +52,33 @@ void readEncoder_right() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  analogWrite(0, 20);
-  analogWrite(1, 20);
-  delay(1000);
-  Serial.print(count_left);
-  Serial.print("\t");
-  Serial.println(count_right);
-  pixels.setPixelColor(0, pixels.Color(100, 0, 0));
-  pixels.show();
+  if(BOOTSEL){
+    count_left = 0;
+    count_right = 0;
+    count_left_buff = 0;
+    count_right_buff = 0;
+    run = 1;
+    pixels.setPixelColor(0, pixels.Color(0, 100, 0));
+    pixels.show();
+    interrupts();
   }
 
+  if(run)
+  {
+    analogWrite(0, 200);
+    analogWrite(1, 200);
+    delay(2000);
+    analogWrite(0, 0);
+    analogWrite(1, 0);
+    //noInterrupts();
+    pixels.setPixelColor(0, pixels.Color(100, 0, 0));
+    pixels.show();
+    run = 0;
+    Serial.print(count_left);
+    Serial.print("\t");
+    Serial.println(count_right);
 
+
+  }
+
+}
